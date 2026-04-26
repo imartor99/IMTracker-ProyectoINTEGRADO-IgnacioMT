2370ae6e6c36b1ca35321845455cfe9b01aca834
imartor99 <imartor769@g.educaand.es>
2026-04-26 12:45:46 +0200

    feat: creacion de modelos de bbdd para usuarios e incidencias

---
+++ incidencias/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import UsuarioPersonalizado, Incidencia, Observacion
+from django.contrib.auth.admin import UserAdmin
+
+#Para crear usuarios desde admin
+class UsuarioPersonalizadoAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Información adicional", {"fields": ("departamento",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Información adicional", {"fields": ("departamento",)}),
+    )
+
+admin.site.register(UsuarioPersonalizado, UsuarioPersonalizadoAdmin)
+admin.site.register(Incidencia)
+admin.site.register(Observacion)
---
+++ incidencias/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+# Create your models here.
+
+class UsuarioPersonalizado(AbstractUser):
+    DEPARTAMENTOS = (
+        ('compras', 'Compras'),
+        ('ventas', 'Ventas'),
+        ('it', 'IT'),
+        ('manager', 'Manager IT'),
+    )
+    departamento = models.CharField(max_length=20, choices=DEPARTAMENTOS, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class Incidencia(models.Model):
+    ESTADOS = (
+        ('pendiente', 'Pendiente'),
+        ('en_curso', 'En curso'),
+        ('faltan_datos', 'Faltan datos'),
+        ('testing', 'Testing'),
+        ('finalizada', 'Finalizada'),
+    )
+
+    PRIORIDADES = (
+        ('alta', 'Alta'),
+        ('media', 'Media'),
+        ('baja', 'Baja'),
+    )
+
+    titulo = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=200)
+    prioridad = models.CharField(max_length=10, choices=PRIORIDADES)
+    imagen = models.ImageField(upload_to='capturas/', null=True, blank=True)
+    creador = models.ForeignKey(UsuarioPersonalizado, related_name='incidencias_creadas', on_delete=models.CASCADE)
+    asignado_a = models.ForeignKey(UsuarioPersonalizado, null=True, blank=True, related_name='incidencias_asignadas', on_delete=models.SET_NULL)
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
+
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_asignacion = models.DateTimeField(null=True, blank=True)
+    fecha_resolucion = models.DateTimeField(null=True, blank=True)
+
+    oculta = models.BooleanField(default=False)  # Para el borrado "no destructivo"
+
+    def __str__(self):
+        return f"#{self.id} - {self.titulo}"
+    
+
+class Observacion(models.Model):
+    incidencia = models.ForeignKey(Incidencia, on_delete=models.CASCADE, related_name='observaciones')
+    autor = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE)
+    texto = models.TextField()
+    fecha = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Obs. #{self.id} para Incidencia #{self.incidencia.id}"
