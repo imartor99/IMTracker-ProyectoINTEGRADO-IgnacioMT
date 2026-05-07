b6b090eca9a4f78a96fbda42b9b0e440279129ce
imartor99 <imartor769@g.educaand.es>
2026-05-07 20:36:57 +0200

    feat(docs): registro ruta para la descarga del PDF de incidencias

--- incidencias/urls.py
+++ incidencias/urls.py
@@ -1,7 +1,7 @@
 # incidencias/urls.py
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import filtrar_incidencias, login_view, logout_view, dashboard, crear_incidencia_ajax, detalle_incidencia, editar_incidencia, borrar_incidencia, contador_no_finalizadas, obtener_incidencia, obtener_usuarios_filtros, obtener_usuarios_it, asignar_incidencia, contactar_it, exportar_csv
+from .views import filtrar_incidencias, login_view, logout_view, dashboard, crear_incidencia_ajax, detalle_incidencia, editar_incidencia, borrar_incidencia, contador_no_finalizadas, obtener_incidencia, obtener_usuarios_filtros, obtener_usuarios_it, asignar_incidencia, contactar_it, exportar_csv, exportar_ticket_pdf
 
 app_name = 'incidencias'
 
@@ -21,5 +21,6 @@ urlpatterns = [
     path('obtener_usuarios_filtros/', obtener_usuarios_filtros, name='obtener_usuarios_filtros'),
     path('contactar_it/', login_required(contactar_it), name='contactar_it'),
     path('exportar/csv/', exportar_csv, name='exportar_csv'),
+    path('incidencia/<int:incidencia_id>/pdf/', exportar_ticket_pdf, name='exportar_ticket_pdf'),
 ]
 
