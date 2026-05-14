e9112cef994e17ed79c4edaa26fbcf7918fdba02
imartor99 <imartor769@g.educaand.es>
2026-05-14 22:01:59 +0200

    feat: habilito rutas para importación masiva y gestión de seguridad de usuarios

--- incidencias/urls.py
+++ incidencias/urls.py
@@ -1,7 +1,7 @@
 # incidencias/urls.py
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import filtrar_incidencias, login_view, logout_view, dashboard, crear_incidencia_ajax, detalle_incidencia, editar_incidencia, borrar_incidencia, contador_no_finalizadas, obtener_incidencia, obtener_usuarios_filtros, obtener_usuarios_it, asignar_incidencia, contactar_it, exportar_csv, exportar_ticket_pdf, consultar_festivos, api_chatbot
+from .views import filtrar_incidencias, login_view, logout_view, dashboard, crear_incidencia_ajax, detalle_incidencia, editar_incidencia, borrar_incidencia, contador_no_finalizadas, obtener_incidencia, obtener_usuarios_filtros, obtener_usuarios_it, asignar_incidencia, contactar_it, exportar_csv, exportar_ticket_pdf, consultar_festivos, api_chatbot, importar_usuarios_csv
 
 app_name = 'incidencias'
 
@@ -24,5 +24,6 @@ urlpatterns = [
     path('incidencia/<int:incidencia_id>/pdf/', exportar_ticket_pdf, name='exportar_ticket_pdf'),
     path('api/festivos/', consultar_festivos, name='consultar_festivos'),
     path('api/chatbot/', api_chatbot, name='api_chatbot'),
+    path('importar/usuarios/csv/', importar_usuarios_csv, name='importar_usuarios_csv'),
 ]
 
