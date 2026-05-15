5bb60d2b66ad4d3b2b3fcaba7a91c9d063a662e2
imartor99 <imartor769@g.educaand.es>
2026-05-16 00:57:32 +0200

    depuro models.py

--- incidencias/models.py
+++ incidencias/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-# Create your models here.
+
 
 class UsuarioPersonalizado(AbstractUser):
     """
